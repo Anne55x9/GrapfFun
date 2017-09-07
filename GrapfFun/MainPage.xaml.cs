e46f5110de38e455f179de69a2241a0e913185ad
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-09-07 18:06:11 +0200

    Implemeteret klassen student i main page i UWP app solution fil. Student attributter er data punkter i charting:LineSeries i xaml koden. To forskellige grafer som viser værdier for individ til forskellige eksamer 2016 og 2017.

--- GrapfFun/MainPage.xaml.cs
+++ GrapfFun/MainPage.xaml.cs
@@ -12,11 +12,38 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+using WinRTXamlToolkit.Controls.DataVisualization.Charting;
+
+
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace GrapfFun
 {
+
+    //Koden til grafen indsættes direkte i test1. 
+
+    public class Student
+    {
+        /// <summary>
+        /// To attributter til klassen student.
+        /// </summary>
+        public string SName { get; set; }
+
+        public int Marks { get; set; }
+
+        public int Marks2 { get; set; }
+
+
+        public Student()
+        {
+
+        }
+
+    }
+
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
@@ -25,6 +52,85 @@ namespace GrapfFun
         public MainPage()
         {
             this.InitializeComponent();
+            GetChartData();
+            GetChartData2();
+
+
         }
+
+
+        private void GetChartData()
+        {
+            List<Student> student = new List<Student>();
+            student.Add(new Student()
+            {
+                SName = "Anders And",
+                Marks = 10
+            });
+
+            student.Add(new Student()
+            {
+                SName = "Mule",
+                Marks = 7
+            });
+
+            student.Add(new Student()
+            {
+                SName = "Rip",
+                Marks = 12
+            });
+
+            student.Add(new Student()
+            {
+                SName = "Helle",
+                Marks = 4
+            });
+
+
+
+           (LineChart.Series[0] as LineSeries).ItemsSource = student;
+            
+
+        }
+
+        private void GetChartData2()
+        {
+            List<Student> student1 = new List<Student>();
+            student1.Add(new Student()
+            {
+                SName = "Anders And",
+                Marks2 = 2
+            });
+
+            student1.Add(new Student()
+            {
+                SName = "Mule",
+                Marks2 = 12
+            });
+
+            student1.Add(new Student()
+            {
+                SName = "Rip",
+                Marks2 = 4
+            });
+
+            student1.Add(new Student()
+            {
+                SName = "Helle",
+                Marks2 = 12
+            });
+
+
+
+            (LineChart.Series[1] as LineSeries).ItemsSource = student1;
+
+
+        }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
     }
 }
